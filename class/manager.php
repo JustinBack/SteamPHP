1041cf64c1dab27ff1ac4f4bb8aba78b74699b88
Justin Back <jb@justinback.com>
2018-06-12 19:45:14 +0200

    Added Documentation Blocks Added Documentation Set variable functions so private Added leaderboards->ResetLeaderboard() Added leaderboards->SetLeaderboardScore() Added leaderboards->FindOrCreateLeaderboard()

--- class/manager.php
+++ class/manager.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * Copyright (c) 2018, Justin Back <jb@justinback.com>
  * All rights reserved.
  */
@@ -13,10 +13,35 @@ namespace justinback\steam;
  * @author Justin Back <jb@justinback.com>
  */
 class manager {
+    /**
+    * Steamworks API Key
+    *
+    */
     private $key = null;
+    
+    /**
+    * Steamworks App ID
+    *
+    */
     private $game = null;
+    
+    /**
+    * SteamID of user
+    *
+    */
     private $steamid = null;
     
+    
+    /**
+    * Construction of the variables steamid, key and game
+    *
+    *
+    * @param string $apikey Steamworks Developer API Key
+    * @param string $game Your Appid
+    * @param string $steamid The SteamID of the user 
+    *
+    * @return void
+    */
     public function __construct($apikey = null, $game = null, $steamid = null)
     {
         $this->set_key($apikey);
@@ -25,23 +50,57 @@ class manager {
         
     }
     
+    /**
+    * Setting API Key from the construct
+    *
+    *
+    * @param string $apikey Steamworks Developer API Key
+    *
+    * @return void
+    */
     private function set_key($apikey)
     {
         $this->key = $apikey;
     }
     
-    public function set_game($game)
+    
+    /**
+    * Setting AppID from the construct
+    *
+    *
+    * @param string $game Your AppID
+    *
+    * @return void
+    */
+    private function set_game($game)
     {
         $this->game = $game;
     }
     
-    public function set_steamid($steamid)
+    
+    /**
+    * Setting SteamID from the construct
+    *
+    *
+    * @param string $steamid The Players SteamID
+    *
+    * @return void
+    */
+    private function set_steamid($steamid)
     {
         $this->steamid = $steamid;
     }
 
     
-    
+    /**
+    * player object.
+    *
+    * @param string $apikey (optional) set a different apikey than the construct
+    * @param string $game (optional) set a different appid than the construct
+    * @param string $steamid (optional) set a different steamid than the construct
+    * 
+    * @return player
+    */
     public function player($apikey = null, $game = null, $steamid = null)
     {
         if($apikey === null){
@@ -56,6 +115,16 @@ class manager {
         return new \justinback\steam\player($apikey,$game,$steamid);
     }
     
+    
+    /**
+    * Game object.
+    *
+    * @param string $apikey (optional) set a different apikey than the construct
+    * @param string $game (optional) set a different appid than the construct
+    * @param string $steamid (optional) set a different steamid than the construct
+    * 
+    * @return game
+    */
     public function game($apikey = null, $game = null, $steamid = null)
     {
         if($apikey === null){

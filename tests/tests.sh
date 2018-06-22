01e2bf6a21aef8b23db79389da655b26c465e991
JBBot <jb@justinback.com>
2018-06-22 19:09:48 +0200

    Updated travis

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,13 +1,14 @@
-DeleteLeaderboards = php $TRAVIS_BUILD_DIR/tests/DeleteLeaderboards.php
-if [$DeleteLeaderboards = false]
+DeleteLeaderboards=php $TRAVIS_BUILD_DIR/tests/DeleteLeaderboards.php
+if [ $DeleteLeaderboards = false ]
 then
     Exit 2
 fi
 
 
 
-GetLeaderboards = php $TRAVIS_BUILD_DIR/tests/GetLeaderboards.php
-if [$GetLeaderboards = false]
+GetLeaderboards=php $TRAVIS_BUILD_DIR/tests/GetLeaderboards.php
+if [ $GetLeaderboards = false ]
 then
     Exit 2
 fi
+read

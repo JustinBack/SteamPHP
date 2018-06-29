6af6fb2891b4ef9dc61b93e6404de6d30405a49c
JBBot <jb@justinback.com>
2018-06-29 11:37:06 +0200

    Updated variables and docs Removed GetAvatarSmall(),GetAvatarMedium() and GetAvatarLarge()

--- class/player.php
+++ class/player.php
@@ -100,43 +100,11 @@ class player {
     * @return string
     */
     public function GetPersonaName(){
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
+        $fgcGetPersonaName = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
+        $oGetPersonaName = json_decode($fgcGetPersonaName);
         
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            return $player->personaname;
-        }
-    }
-    
-    /**
-    * Get the Avatar in small
-    *
-    * @deprecated Use GetAvatar() instead
-    *
-    * @return string
-    */
-    public function GetAvatarSmall(){
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
-        
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            return $player->avatar;
-        }
-    }
-    
-    /**
-    * Get the Avatar in medium
-    *
-    * @deprecated Use GetAvatar() instead
-    *
-    * @return string
-    */
-    public function GetAvatarMedium(){
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
-        
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            return $player->avatarmedium;
+        foreach($oGetPersonaName->response->players as $oPlayer){
+            return $oPlayer->personaname;
         }
     }
     
@@ -149,15 +117,15 @@ class player {
     * @return object
     */
     public function GetAvatar(){
-        $object = new \stdClass();
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
+        $oObject = new \stdClass();
+        $fgcGetAvatar = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
+        $oGetAvatar = json_decode($fgcGetAvatar);
         
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            $object->small = $player->avatar;
-            $object->medium = $player->avatarmedium;
-            $object->full = $player->avatarfull;
-            return $object;
+        foreach($oGetAvatar->response->players as $oPlayer){
+            $oObject->small = $oPlayer->avatar;
+            $oObject->medium = $oPlayer->avatarmedium;
+            $oObject->full = $oPlayer->avatarfull;
+            return $oObject;
         }
     }
     
@@ -171,28 +139,11 @@ class player {
     * @return string
     */
     public function GetRealName(){
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
+        $fgcGetRealName = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
+        $oGetRealName = json_decode($fgcGetRealName);
         
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            return $player->realname;
-        }
-    }
-    
-    
-    /**
-    * Get the Avatar in full
-    *
-    * @deprecated Use GetAvatar() instead
-    *
-    * @return string
-    */
-    public function GetAvatarFull(){
-        $req_players = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=".$this->key."&steamids=".$this->steamid);
-        $GetNumberOfCurrentPlayers = json_decode($req_players);
-        
-        foreach($GetNumberOfCurrentPlayers->response->players as $player){
-            return $player->avatarfull;
+        foreach($oGetRealName->response->players as $oPlayer){
+            return $oPlayer->realname;
         }
     }
     
@@ -203,32 +154,32 @@ class player {
     * It is intended for unreliable data from peers in the game ( semi-trusted sources ). The back-end that reports the data should ensure that both parties are authenticated, but the data in itself is treated as hearsay. Optional parameters may be used to encode the type of cheating that is suspected or additional evidence ( an identifier pointing to the match/demo for further review )
     *
     * @param string $sSteamidreporter (Optional) The Steam ID of the user or game server who is reporting the cheating.
-    * @param string $appdata (Optional) App specific data about the type of cheating set by developer. (ex 1 = Aimbot, 2 = Wallhack, 3 = Griefing)
-    * @param bool $heuristic (Optional) Extra information about the source of the cheating - was it a heuristic.
-    * @param bool $detection (Optional) Extra information about the source of the cheating - was it a detection.
-    * @param bool $playerreport (Optional) Extra information about the source of the cheating - was it a player report.
-    * @param bool $noreportid (Optional) Don't return reportid. This should only be passed if you don't intend to issue a ban based on this report.
+    * @param string $sAppData (Optional) App specific data about the type of cheating set by developer. (ex 1 = Aimbot, 2 = Wallhack, 3 = Griefing)
+    * @param bool $bHeuristic (Optional) Extra information about the source of the cheating - was it a heuristic.
+    * @param bool $bDetection (Optional) Extra information about the source of the cheating - was it a detection.
+    * @param bool $bPlayerReport (Optional) Extra information about the source of the cheating - was it a player report.
+    * @param bool $bNoReportId (Optional) Don't return reportid. This should only be passed if you don't intend to issue a ban based on this report.
     * @param int $iGamemode (Optional) Extra information about state of game - was it a specific type of game play or game mode. (0 = generic)
-    * @param int $suspicionstarttime (Optional) Extra information indicating how far back the game thinks is interesting for this user. Unix epoch time (time since Jan 1st, 1970).
-    * @param int $severity (Optional) Level of severity of bad action being reported. Scale set by developer.
+    * @param int $iSuspicionStartTime (Optional) Extra information indicating how far back the game thinks is interesting for this user. Unix epoch time (time since Jan 1st, 1970).
+    * @param int $iSeverity (Optional) Level of severity of bad action being reported. Scale set by developer.
     * 
     * 
     * @return anticheat
     */
-    public function ReportPlayerCheating($sSteamidreporter = 0, $appdata = 0, $heuristic = false, $detection = false, $playerreport = false, $noreportid = false, $iGamemode = 0, $suspicionstarttime = 0, $severity = 0){
-        $options = array(
+    public function ReportPlayerCheating($sSteamidreporter = 0, $sAppData = 0, $bHeuristic = false, $bDetection = false, $bPlayerReport = false, $bNoReportId = false, $iGamemode = 0, $iSuspicionStartTime = 0, $iSeverity = 0){
+        $aOptions = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
-                'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, 'steamid' => $this->steamid, 'steamidreporter' => $sSteamidreporter, 'appdata' => $appdata, 'heuristic' => $heuristic, 'detection' => $detection, 'playerreport' => $playerreport, 'noreportid' => $noreportid, 'gamemode' => $iGamemode, 'suspicionstarttime' => $suspicionstarttime, 'severity' => $severity ))
+                'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, 'steamid' => $this->steamid, 'steamidreporter' => $sSteamidreporter, 'appdata' => $sAppData, 'heuristic' => $bHeuristic, 'detection' => $bDetection, 'playerreport' => $bPlayerReport, 'noreportid' => $bNoReportId, 'gamemode' => $iGamemode, 'suspicionstarttime' => $iSuspicionStartTime, 'severity' => $iSeverity ))
             )
         );
-        $context  = stream_context_create($options);
-        $req_players = file_get_contents("https://partner.steam-api.com/ICheatReportingService/ReportPlayerCheating/v1/", false, $context);
-        $response = json_decode($req_players);
+        $cContext  = stream_context_create($aOptions);
+        $fgcReportPlayerCheating = file_get_contents("https://partner.steam-api.com/ICheatReportingService/ReportPlayerCheating/v1/", false, $cContext);
+        $oReportPlayerCheating = json_decode($fgcReportPlayerCheating);
        
         
-        return new \justinback\steam\anticheat($response->response->reportid, $this->key, $this->game, $this->steamid);
+        return new \justinback\steam\anticheat($oReportPlayerCheating->response->reportid, $this->key, $this->game, $this->steamid);
     }
     
     
@@ -237,33 +188,33 @@ class player {
     *
     * 
     * @param string $sSteamidreporter SteamID of user doing the reporting
-    * @param int $abuseType Abuse type code (see EAbuseReportType enum)
-    * @param int $contentType Content type code (see ECommunityContentType enum)
-    * @param string $description Narrative from user
-    * @param string gid (optional) GID of related record (depends on content type)
+    * @param int $iAbuseType Abuse type code (see EAbuseReportType enum)
+    * @param int $iContentType Content type code (see ECommunityContentType enum)
+    * @param string $sDescription Narrative from user
+    * @param string $sGid (optional) GID of related record (depends on content type)
     * 
     * @return object
     */
-    public function ReportAbuse($sSteamidreporter, $abuseType, $contentType, $description, $gid = null){
-        $options = array(
+    public function ReportAbuse($sSteamidreporter, $iAbuseType, $iContentType, $sDescription, $sGid = null){
+        $aOptions = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
-                'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, 'steamidTarget' => $this->steamid, 'steamidActor' => $sSteamidreporter, 'abuseType' => $abuseType, 'contentType' => $contentType, 'description' => $description, 'gid' => $gid))
+                'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, 'steamidTarget' => $this->steamid, 'steamidActor' => $sSteamidreporter, 'abuseType' => $iAbuseType, 'contentType' => $iContentType, 'description' => $sDescription, 'gid' => $sGid))
             )
         );
-        $context  = stream_context_create($options);
-        $req_players = file_get_contents("https://partner.steam-api.com/ISteamCommunity/ReportAbuse/v1/", false, $context);
-        $response = json_decode($req_players);
-        $obj = new \stdClass();
-        if($response->result>success == 1){
-            $obj->success = true;
-            $obj->message = null;
-            return $obj;
+        $cContext  = stream_context_create($aOptions);
+        $fgcReportAbuse = file_get_contents("https://partner.steam-api.com/ISteamCommunity/ReportAbuse/v1/", false, $cContext);
+        $oReportAbuse = json_decode($fgcReportAbuse);
+        $oObj = new \stdClass();
+        if($oReportAbuse->result>success == 1){
+            $oObj->success = true;
+            $oObj->message = null;
+            return $oObj;
         }
-        $obj->success = false;
-        $obj->message = $response->result->message;
-        return $obj;
+        $oObj->success = false;
+        $oObj->message = $oReportAbuse->result->message;
+        return $oObj;
     }
     
     
@@ -278,18 +229,18 @@ class player {
     * @return bool
     */
     public function RemovePlayerGameBan(){
-        $options = array(
+        $aOptions = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
                 'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, 'steamid' => $this->steamid))
             )
         );
-        $context  = stream_context_create($options);
-        $req_players = file_get_contents("https://partner.steam-api.com/ICheatReportingService/RemovePlayerGameBan/v1/", false, $context);
-        $response = json_decode($req_players);
+        $cContext  = stream_context_create($aOptions);
+        $fgcRemovePlayerGameBan = file_get_contents("https://partner.steam-api.com/ICheatReportingService/RemovePlayerGameBan/v1/", false, $cContext);
+        $oRemovePlayerGameBan = json_decode($fgcRemovePlayerGameBan);
         
-        if(count($response->response) == 0){
+        if(count($oRemovePlayerGameBan->response) == 0){
             return false;
         }
         return true;
@@ -304,24 +255,24 @@ class player {
     * @return ugc array
     */
     public function EnumerateUserPublishedFiles(){
-        $options = array(
+        $aOptions = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
                 'content' => http_build_query(array('key' => $this->key, 'appid' => (int)$this->game, "steamid" => $this->steamid))
             )
         );
-        $context  = stream_context_create($options);
-        $req_players = file_get_contents("https://partner.steam-api.com/ISteamRemoteStorage/EnumerateUserPublishedFiles/v1/", false, $context);
-        $response = json_decode($req_players);
+        $cContext  = stream_context_create($aOptions);
+        $fgcEnumerateUserPublishedFiles = file_get_contents("https://partner.steam-api.com/ISteamRemoteStorage/EnumerateUserPublishedFiles/v1/", false, $cContext);
+        $oEnumerateUserPublishedFiles = json_decode($fgcEnumerateUserPublishedFiles);
        
-        $list = array();
+        $aList = array();
         
-        foreach ($response->response->files as $file) {
-            array_push($list, new \justinback\steam\ugc($file->publishedfileid, $this->key, $this->game, $this->steamid));
+        foreach ($oEnumerateUserPublishedFiles->response->files as $oFile) {
+            array_push($aList, new \justinback\steam\ugc($oFile->publishedfileid, $this->key, $this->game, $this->steamid));
         }
         
-        return $list;
+        return $aList;
     }
     
     
